68fc16e1dafad53c1c5065c7fd423d32ab6d425d
Animantra <t20227893@gmail.com>
2026-04-27 00:41:20 +0500

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,13 @@
 variable "instance_type" { default = "c7i-flex.large" }
 variable "ami_id"        { default = "ami-0014ce3e52359afbd" } 
 variable "key_name"      { default = "my-key" }
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
