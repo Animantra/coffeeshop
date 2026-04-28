be758017d407d3db36b6da6108e21dbc239bb19d
Animantra <t20227893@gmail.com>
2026-04-28 14:58:48 +0500

    up

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,30 +36,7 @@ resource "aws_instance" "app_server" {
     volume_size = 20  
     volume_type = "gp3" 
   }
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update
-              sudo apt-get upgrade -y
-              sudo fallocate -l 2G /swapfile
-              sudo chmod 600 /swapfile
-              sudo mkswap /swapfile
-              sudo swapon /swapfile
-              echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-
-              sudo apt-get install -y docker.io docker-compose-v2 git curl
-              sudo systemctl start docker
-              sudo systemctl enable docker
-              sudo usermod -aG docker ubuntu
-
-    
-              cd /home/ubuntu
-              git clone https://${var.github_token}@github.com/Animantra/coffeeshop.git
-              
-              sudo chown -R ubuntu:ubuntu /home/ubuntu/coffeeshop
-
-              # cd /home/ubuntu/coffeeshop
-              # sudo docker compose up -d
-              EOF
+  user_data = file("scripts/script.sh")
 
   tags = {
     Name = "CoffeeShop-SRE-Server"
---
+++ terraform/scripts/script.sh
@@ -0,0 +1,20 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+sudo apt-get install -y docker.io docker-compose-v2 git curl
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
+
+cd /home/ubuntu
+git clone https://github.com/Animantra/coffeeshop
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu/coffeeshop
+
+cd /home/ubuntu/coffeeshop
+sudo docker compose up -d
